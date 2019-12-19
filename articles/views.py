36cda8841899f49cfed25442df6ba97baf4c2bad
priyank <ipriyank007@hotmail.com>
2019-12-19 18:35:22 +0530

    views added

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Article
 
 def home(request):
-    return render(request, 'main/index.html')
+    articles = Article.objects.all().order_by('-published_on')
+    if len(articles) > 5:
+        articles = articles[:5]
+    
+    context = {
+        'articles': articles,
+    }
+
+    return render(request, 'main/index.html', context)
